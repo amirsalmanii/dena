d0ee6090d8a5b531be6f6af7378c4f404412de2e
amirSalmani <salmaniamir13@gmail.com>
2022-03-20 22:43:05 +0330

    add new fields and add to admin

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User as CustomUser
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    model = CustomUser
+    list_display = ('email', 'is_staff', 'is_active',)
+    list_filter = ('email', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'first_name', 'last_name')}),
+        ('Address', {'fields': ('state', 'city', 'address', 'plate', 'zip_code', 'phone_number')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+admin.site.register(CustomUser, UserAdmin)
--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+from .managers import CustomUserManager
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    first_name = models.CharField(max_length=200, blank=True)
+    last_name = models.CharField(max_length=200, blank=True)
+    email = models.EmailField(_('email address'), unique=True)
+
+    # address fields
+    state = models.CharField(max_length=200, blank=True)
+    city = models.CharField(max_length=200, blank=True)
+    address = models.TextField(blank=True)
+    plate = models.CharField(max_length=5, blank=True) # pelak khane
+    zip_code = models.CharField(max_length=10, blank=True)
+    phone_number = models.CharField(max_length=11, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
