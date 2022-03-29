6819d83ed0571f00e47c76e9267ff1ece1455217
amirSalmani <salmaniamir13@gmail.com>
2022-03-30 00:20:57 +0430

    add url for view

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('login/user/', views.LoginUserView.as_view(), name='login_user'),
     path('register/user/', views.RegisterUserView.as_view(), name='register_user'),
-    path('create/user/', views.CreateUserView.as_view(), name='create_user'),
     path('users/', views.UsersListView.as_view(), name='users_list'),
     path('user/<int:pk>/', views.UserDetailUpdateView.as_view(), name='user_detail_update'),
+    path('user/forget/', views.ForgetPassword.as_view(), name='update_user_password'),
 ]
