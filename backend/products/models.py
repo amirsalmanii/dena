28d5106bf8e36283c287b9f7a11b520a9641d664
amirSalmani <salmaniamir13@gmail.com>
2022-03-30 15:47:01 +0430

    migrations added

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=254, unique=True, allow_unicode=True, blank=True)
+
+    class Meta:
+        ordering = ('-id',)
+    
+    def save(self, *args, **kwargs):
+        self.slug = self.name.replace(' ', '-')
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+    
+
