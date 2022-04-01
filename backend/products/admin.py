1fc7736b6dc5f1f79bf6b43efb42bcca4e337a2d
amirSalmani <salmaniamir13@gmail.com>
2022-04-01 11:29:12 +0430

    admin models

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Category)
+
+@admin.register(models.Category)
+class AdminCategory(admin.ModelAdmin):
+    list_display = ["name", "parent"]
+
+
+@admin.register(models.Product)
+class AdminCategory(admin.ModelAdmin):
+    list_display = ["name", "price", "company_price", "quantity", "hide"]
