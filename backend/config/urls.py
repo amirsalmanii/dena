534432e266b2e28add24b353673f4a9c3a76ec15
amirSalmani <salmaniamir13@gmail.com>
2022-05-12 13:55:44 +0430

    ckeditor urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('api/v1/', include('accounts.urls')),
     path('api/v1/', include('products.urls')),
     path('api/v1/', include('news.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
