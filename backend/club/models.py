68c4c974ff3508f01583739513fefe5ff381d11f
amirSalmani <salmaniamir13@gmail.com>
2022-06-12 18:28:34 +0430

    add model

--- backend/club/models.py
+++ backend/club/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Club(models.Model):
+    first_name = models.CharField(max_length=200, null=True, blank=True)
+    last_name = models.CharField(max_length=200, null=True, blank=True)
+    age = models.CharField(max_length=200, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    phone_number = models.CharField(max_length=200, null=True, blank=True)
+    car = models.CharField(max_length=200, null=True, blank=True)
+    license_cart = models.ImageField(upload_to='license/', null=True, blank=True)
+    car_image = models.ImageField(upload_to='cars/', null=True, blank=True)
