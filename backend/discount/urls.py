1679e04da5749ef9d8d5c3df481805fc924b4ac8
amirSalmani <salmaniamir13@gmail.com>
2022-06-12 18:31:39 +0430

    add view and url

--- backend/discount/urls.py
+++ backend/discount/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('discount/list/', views.DiscountListView.as_view(), name='discount_list'),
     path('discount/create/', views.DiscountCreateView.as_view(), name='discount_create'),
-    path('discount_update/<int:pk>/', views.DiscountUpdateView.as_view(), name='discount_update'),
-    path('discount_detail/<int:pk>/', views.DiscountDetailView.as_view(), name='discount_detail'),
+    # path('discount_update/<int:pk>/', views.DiscountUpdateView.as_view(), name='discount_update'),
+    # path('discount_detail/<int:pk>/', views.DiscountDetailView.as_view(), name='discount_detail'),
     path('discount_delete/<int:pk>/', views.DiscountDeletView.as_view(), name='discount_delete'),
 ]
--- backend/discount/views.py
+++ backend/discount/views.py
@@ -9,9 +9,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Discount
 from . import serializers
+from accounts.views import MyPagination
 
 
 class DiscountListView(ListAPIView):
+    pagination_class = MyPagination
     queryset = Discount.objects.all()
     serializer_class = serializers.DiscountListSerializer
 
@@ -20,12 +22,12 @@ class DiscountCreateView(APIView):
     def post(self, request):
         serializer = serializers.DiscountCreateSerializer(data=request.data)
         if serializer.is_valid():
-            print('yes')
             data = serializer.validated_data
             products = data['products']
             percent = data['discount_percent']
             for pr in products:
                 pr.price_after_discount = pr.price - (1 / 100) * percent * pr.price
+                pr.discount_percent = percent
                 pr.save()
             serializer.save()
             return Response(serializer.data)
@@ -43,6 +45,7 @@ class DiscountUpdateView(APIView):
             percent = data['discount_percent']
             for pr in products:
                 pr.price_after_discount = pr.price - (1 / 100) * percent * pr.price
+                pr.discount_percent = percent
                 pr.save()
             serializer.save()
             return Response(serializer.data)
